2176ca642c309df6881073fb6546e65aae6dc037
Vanessa Sergeon <vsergeon@gmail.com>
2014-07-09 14:29:25 -0400

    repeat and factorial functions working but not passing test.

--- 02_loops/Loops.js
+++ 02_loops/Loops.js
@@ -1,21 +1,22 @@
+// repeat works but will not pass test. why?
 function repeat(word, number) {
 	var repeated = "";
-		for (var i = 0; i <= number; i++) {
-			repeated += "word";
+		for (var i = 0; i <= number -1; i++) {
+			repeated += word;
 		} // end for loop
 	return repeated;
-} // end repeat function
+} // end repeat function.
 
 function join(array, delimiter) {
-	var joint = '';
-	if (array, delimiter) {
+	var joint = "";
+	if (delimiter) {
 		for (var i = 0; i <= array.length - 1; i++) {
 			joint = joint + array[i];
-				for (var i = 0; i <= array.length - 2; i++) {
+				for (var x = 0; x <= array.length - 2; x++) {
 				joint = joint + delimiter;	
 		}; // end for statement
-		} else if(array) {
-			for (var i = 0; i <= array.length - 1; i++) {
+		} else if (array) {
+			for (var y = 0; y <= array.length - 1; y++) {
 				joint = joint + array[i];
 			}
 		} else {
@@ -36,13 +37,18 @@ function paramify(hash) {
 	// Use for in loop
 }; // end paramify functiion
 
+// factorial works.
 function factorial(number) {
-	var fact = 1;
+    if (number === 0 || number == 1) {
+        return 1;
+    } else {
+    var fact = 1;
 	for (var i = number; i >= 1; i --) {
 		fact = fact * i;
-	};
+	} // end for loop
 	return fact;
-}; // end factorial function
+    } // end if else statement
+} // end factorial function
 
 function result(concat_string) {
 	var string = concat_string.join('');
