344781a156219773f6cabdcd2087fbd19034fefb
Vanessa Sergeon <vsergeon@gmail.com>
2014-07-10 11:48:51 -0400

    comment out code. only testing repeat function

--- 02_loops/Loops.js
+++ 02_loops/Loops.js
@@ -1,3 +1,15 @@
+// passes test
+function repeat(word, number) {
+	var repeated = "";
+		for (var i = 0; i <= number -1; i++) {
+			repeated += word;
+		} // end for loop
+	return repeated;
+} // end repeat function.
+
+
+/*
+
 // repeat works but will not pass test. why?
 function repeat(word, number) {
 	var repeated = "";
@@ -7,17 +19,17 @@ function repeat(word, number) {
 	return repeated;
 } // end repeat function.
 
-function join(array, delimiter) {
+function join(object, delimiter) {
 	var joint = "";
 	if (delimiter) {
-		for (var i = 0; i <= array.length - 1; i++) {
-			joint = joint + array[i];
-				for (var x = 0; x <= array.length - 2; x++) {
+		for (var i = 0; i <= object.length - 1; i++) {
+			joint = joint + object[i];
+				for (var x = 0; x <= object.length - 2; x++) {
 				joint = joint + delimiter;	
 		}; // end for statement
-		} else if (array) {
-			for (var y = 0; y <= array.length - 1; y++) {
-				joint = joint + array[i];
+		} else if (object) {
+			for (var y = 0; y <= object.length - 1; y++) {
+				joint = joint + object[i];
 			}
 		} else {
 			joint = "";
@@ -25,10 +37,10 @@ function join(array, delimiter) {
 	return joint;
 }; // end join function
 
-function sum(array) {
+function sum(object) {
 	var add = 0;
-	for (var i = 0; i <= array.legnth; i++) {
-		add = add + array[i];
+	for (var i = 0; i <= object.legnth; i++) {
+		add = add + object[i];
 	};
 	return add;
 }; // end function sum
@@ -50,8 +62,9 @@ function factorial(number) {
     } // end if else statement
 } // end factorial function
 
-function result(concat_string) {
-	var string = concat_string.join('');
-	return string;
+function concat_string() {
+	var result = concat_string.join('');
+	return result;
 }; // end result function
 
+*/
