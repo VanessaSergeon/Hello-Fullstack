0f62d0677aefc5dfe93baa0b99ef4acb27531138
Vanessa Sergeon <vsergeon@gmail.com>
2014-07-15 17:46:41 -0400

    Passing 23/24 test specs.

--- 02_loops/Loops.js
+++ 02_loops/Loops.js
@@ -6,13 +6,33 @@ function repeat(word, number) {
 	return repeated;
 } // end repeat function.
 
+// This version of join returns undefined when array is empty.
 function join(array, delimiter) {
     var str = "";
-    for(i=0; i <= array.length - 1; i++){
-        str += array[i];
+    for(i=0; i <= array.length - 2; i++){
+        str += array[i] + (delimiter || "");
     }
+    str += array[array.length - 1];
     return str;
     } // end join
+
+
+// function join(array, delimiter) {
+//     var str = "";
+//     debugger;
+
+//     if(typeof delimiter === 'undefined'){
+//         delimiter = "";
+//     }
+
+
+//     for(var i=0; i <= array.length; i++){
+//         str += array[i];
+//         if( i !== array.length - 1)
+//             str+=delimiter;   
+//     }
+//     return str;
+//     } // end join
 
 function factorial(number) {
     if (number === 0 || number == 1) {
@@ -47,58 +67,15 @@ function sum(object) {
 	return add;
 } // end function sum
 
-function concat_string(args) {
-    var arr = Array.prototype.slice.call(arguments);
-    var arr1 = [];
-        for(i=0; i<=arr.length; i++) {
-        if(arr[i] !== " ") {
-    	    arr1.push(arr[i]);
-    	}
-    }
-    var str = arr1.join(' ');
-	return str;
+// concat_string works on repl_it. do not know why specs dont pass.
+function concat_string() {
+    return Array.prototype.slice.call(arguments).join('');
+ //    var arr1 = [];
+ //        for(i=0; i<=arr.length; i++) {
+ //        if(arr[i] !== " ") {
+ //    	    arr1.push(arr[i]);
+ //    	}
+ //    }
+ //    var str = arr1.join(' ');
+	// return str;
 } // end concat_string
-
-
-/*
-
-// causes all code to fail test when un-commented.
-function join(object, delimiter) {
-	var joint = "";
-	if (delimiter) {
-		for (var i = 0; i <= object.length - 1; i++) {
-			joint = joint + object[i];
-				for (var x = 0; x <= object.length - 2; x++) {
-				joint = joint + delimiter;	
-		}; // end for statement
-		} else if (object) {
-			for (var y = 0; y <= object.length - 1; y++) {
-				joint = joint + object[i];
-			}
-		} else {
-			joint = "";
-	} // end if, else if statement
-	return joint;
-}; // end join function
-
-
-function paramify(hash) {
-    var array = [];
-    var propArray = Object.keys(hash); // array of properties
-    for(p in hash) {
-        var plus = propArray[p] + '=' + hash[propArray[p]];
-        array.push(plus);
-    } // end for in loop
-    var sort = array.sort();
-    var sum = array.join('&');
-    return sum;
-} // end paramify
-
-
-
-function concat_string(argument) {
-	var result = argument.join('');
-	return result;
-}; // end result function
-
-*/
