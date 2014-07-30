899ac79291bc502711f2bb6596d26c6e4202453c
Vanessa Sergeon <vsergeon@gmail.com>
2014-07-30 18:32:51 -0400

    Passing. Instructor solutions now included

--- 08_mammals/mammal.js
+++ 08_mammals/mammal.js
@@ -1,26 +1,37 @@
 function Mammal(name){
   this.name = name;
   this.offspring =[];
-  this.sayHello = function(){return "My name is " + name + ", I'm a Mammal";}
-  this.haveBaby = function(){
+  // this.sayHello = function(){return "My name is " + this.name + ", I'm a Mammal";
+  // }
+  // this.haveBaby = function(){
+  //   var baby = new Mammal("Baby " + this.name)
+  //   this.offspring.push(baby);
+  //   return baby;
+  // }
+}
+
+Mammal.prototype.sayHello = function(){
+  return "My name is " + this.name + ", I'm a Mammal";
+}
+
+Mammal.prototype.haveBaby = function(){
     var baby = new Mammal("Baby " + this.name)
     this.offspring.push(baby);
     return baby;
-  } // not sure what this is asking
-}
+  }
 
 function Cat(name, color){
-  this.name = name;
+  //this.name = name;
+  Mammal.call(this, name);
   this.color = color;
 }
 
-// Cat.prototype = Object.create(Mammal.prtotype);
-// Cat.prototype.constructor = Mammal;
-
-Cat.prototype = new Mammal();
+// Cat.prototype = new Mammal();
+Cat.prototype = Object.create(Mammal.prototype);
 Cat.prototype.constructor = Cat;
+  // using constructor will ensure that the prototype is not Mammal
 Cat.prototype.haveBaby = function(color){
-  var baby = new Mammal("Baby " + this.name)
+  var baby = new Cat("Baby " + this.name)
   baby.color = color;
   this.offspring.push(baby);
   return baby;
